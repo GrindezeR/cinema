67615980adc871896402949a436da1a10c4f32da
GrindezeR <valariot@gmail.com>
2023-03-12 18:54:42 +0300

    config prettier and import formatter lib + env, next config

--- app/components/application/application.tsx
+++ app/components/application/application.tsx
@@ -1,31 +1,31 @@
 import { useEffect, useState } from 'react';
 
 type TData = {
-  name: string;
+	name: string;
 };
 
 const Application = () => {
-  const [data, setData] = useState<TData>();
+	const [data, setData] = useState<TData>();
 
-  console.warn('RENDER');
+	console.warn('RENDER');
 
-  useEffect(() => {
-    const getData = async () => {
-      const response = await (
-        await fetch(`http://localhost:3000/api/hello`)
-      ).json();
-      setData(response);
-    };
-    getData();
-  }, []);
+	useEffect(() => {
+		const getData = async () => {
+			const response = await (
+				await fetch(`http://localhost:3000/api/hello`)
+			).json();
+			setData(response);
+		};
+		getData();
+	}, []);
 
-  if (!data) return <span>...LOADING...</span>;
+	if (!data) return <span>...LOADING...</span>;
 
-  return (
-    <>
-      <div className='test-border'>Name: {data.name}</div>
-    </>
-  );
+	return (
+		<>
+			<div className="test-border">Name: {data.name}</div>
+		</>
+	);
 };
 
 export default Application;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Application from '../app/components/application/application';
 
 export default function Home() {
-  return (
-    <>
-      <Application />
-    </>
-  );
+	return (
+		<>
+			<Application />
+		</>
+	);
 }
